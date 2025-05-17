5ef441a66b0fd8bcdcc77704afff21d25c6fade8
william-p-silva <josepereirawilliam@gmail.com>
2025-05-17 19:12:28 -0300

    Funções

--- teste/teste/Program.cs
+++ teste/teste/Program.cs
@@ -10,8 +10,25 @@ namespace teste
     {
         static void Main(string[] args)
         {
-            int mod = 5 / 2;
-            Console.WriteLine(mod);
+            float num = 0.0f;
+            Apresentação();
+            num = float.Parse(Console.ReadLine());
+            Operações(num);
+        }
+
+        static void Apresentação()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Este programa vai realizar algumas operações matematicas");
+            Console.WriteLine("Vamos começar :)");
+        }
+        static void Operações(float n)
+        {
+            Console.WriteLine($"O dobro do número {n} é {n * 2}");
+            Console.WriteLine($"A metade do número {n} é {n / 2}");
+            Console.WriteLine($"O quadrado do número {n} é {n * n}");
+            Console.WriteLine($"A raiz quadrada do número {n} é {Math.Sqrt(n)}");
+            Console.WriteLine($"10% do número {n} é {n * 0.10}");
         }
     }
 }
